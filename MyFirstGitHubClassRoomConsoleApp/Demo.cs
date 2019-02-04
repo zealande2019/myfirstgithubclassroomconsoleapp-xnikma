36ea0220596db49686f5b5fd2f0097d66c46d116
Nikolai Lewandowski <xnikma@gmail.com>
2019-02-04 16:32:18 +0100

    Tilføjede et par properties

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        const string first = "first";
+        int second = "2";
+        bool third = true;
+        byte size = 255;
+    }
+}
